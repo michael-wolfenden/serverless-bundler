08ca451160fdc591a6f9db075e8263b5cbe7790d
Michael Wolfenden <michael.wolfenden@gmail.com>
2020-09-29 09:38:41 +1000

    feat: add jest-partial

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,13 +1,15 @@
 const {defaults} = require('jest-config')
 const {fromRoot, hasFile} = require('../utils')
 
+const customSetupFile = hasFile('testing/setup-jest.js')
+  ? [fromRoot('testing/setup-jest.js')]
+  : []
+
 const config = {
   ...defaults,
   roots: [fromRoot('.')],
   testEnvironment: 'node',
-  setupFilesAfterEnv: hasFile('testing/setup-jest.js')
-    ? [fromRoot('testing/setup-jest.js')]
-    : [],
+  setupFilesAfterEnv: ['jest-partial', ...customSetupFile],
 }
 
 module.exports = config
