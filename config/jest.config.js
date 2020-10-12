57be852676e092fe66dce3001eb9498bd86e6169
Michael Wolfenden <michael.wolfenden@gmail.com>
2020-10-12 11:12:10 +1000

    feature: allow custom globalSetup & globalTeardown
    The following files will be respected:
    src/testing/jest-global-setup.js
    src/testing/jest-global-teardown.js
    src/testing/jest-setup-files-after-env.js
    
    testing/jest-global-setup.js
    testing/jest-global-teardown.js
    testing/jest-setup-files-after-env.js

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,15 +1,25 @@
 const {defaults} = require('jest-config')
 const {fromRoot, hasFile} = require('../utils')
 
-const customSetupFile = hasFile('src/testing/setup-jest.js')
-  ? [fromRoot('src/testing/setup-jest.js')]
-  : []
+const getPathFromRoot = filename => {
+  const startingFromSrcPath = `src/testing/${filename}`
+  if (hasFile(startingFromSrcPath)) return fromRoot(startingFromSrcPath)
+
+  const startingFromTestingPath = `testing/${filename}`
+  if (hasFile(startingFromTestingPath)) return fromRoot(startingFromTestingPath)
+
+  return null
+}
 
 const config = {
   ...defaults,
   roots: [fromRoot('.')],
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['jest-partial', ...customSetupFile],
+  globalSetup: getPathFromRoot('jest-global-setup.js'),
+  globalTeardown: getPathFromRoot('jest-global-teardown.js'),
+  setupFilesAfterEnv: [getPathFromRoot('jest-setup-files-after-env.js')].filter(
+    Boolean,
+  ),
 }
 
 module.exports = config
